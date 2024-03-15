15fc821090b63455fc8c1037e741aecc74f85d10
Jesus Garcia <jesus.garcia@open2000.cl>
2024-03-15 11:20:20 -0300

    (feat) initial commit

---
+++ src/app/modules/shared/properties/shared.properties.ts
@@ -0,0 +1,8 @@
+export class SharedProperties {
+
+    public static readonly ROL_ADMIN = 'ADMINISTRADOR';
+    public static readonly ROL_JEFE = 'JEFE';
+    public static readonly ROL_SECRETARIA = 'SECRETARIA';
+    public static readonly ROL_GERENTE = 'GERENTE';
+    public static readonly ROL_VENDEDOR = 'VENDEDOR';
+}
